170e10d5016b9af7e141968dedd69ec1012628b2
IacoSimoncini <iaco.simoncini@gmail.com>
2019-10-07 13:56:57 +0200

    Commenti classe NottiNazione e creazione classe DownloadTSV

--- src/main/java/com/momoiaco/progetto/ProgettoApplication.java
+++ src/main/java/com/momoiaco/progetto/ProgettoApplication.java
@@ -1,6 +1,5 @@
 package com.momoiaco.progetto;
 
-import com.momoiaco.progetto.servizi.DownloadTSV;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,8 +9,5 @@ public class ProgettoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProgettoApplication.class, args);
 	}
-		private String indirizzo = "http://data.europa.eu/euodp/data/api/3/action/package_show?id=CLfXgIIz02XfA2MTjWgjSQ";
-		new DownloadTSV(indirizzo);
-
 
 }
--- src/main/java/com/momoiaco/progetto/modello/NottiNazione.java
+++ src/main/java/com/momoiaco/progetto/modello/NottiNazione.java
@@ -1,36 +1,75 @@
 package com.momoiaco.progetto.modello;
 
+import java.io.Serializable;
 
-public class NottiNazione {
+/**
+ * @author Iacopo Simoncini, Mohammad Wafa
+ * Classe modellante dataset
+ */
+public class NottiNazione implements Serializable {     //Serializable permette di salvare gli oggetti della classe su file
 
-    private char freq;
     private String c_resid, unit, nace_r2, geo;
-    private double[] valori = new double[12];
+    private double[] valori;
+    public static final int differenza_anni = 12;
 
-    //Costruttore
-    public NottiNazione(char freq, String c_resid, String unit, String nace_r2, String geo, double valori[]){
-        this.freq = freq;
+    /**
+     * Costruttore della classe NottiNazione
+     * @param c_resid
+     * @param unit
+     * @param nace_r2
+     * @param geo
+     * @param valori
+     */
+    public NottiNazione (String c_resid, String unit, String nace_r2, String geo, double[] valori) {
         this.c_resid = c_resid;
         this.unit = unit;
         this.nace_r2 = nace_r2;
         this.geo = geo;
-        for(int i = 0 ; i < 12; i++){
-            this.valori[i] = valori[i];
-        }
+        this.valori = valori;
     }
 
-    public char getFreq() { return freq; }
+    /**
+     * Metodi get per C_resid
+     * @return
+     */
+    public String getC_resid() {
+        return c_resid;
+    }
 
-    public String getC_resid() { return c_resid; }
+    /**
+     * Metodo get per Unit
+     * @return
+     */
+    public String getUnit() {
+        return unit;
+    }
 
-    public String getUnit() { return unit; }
+    /**
+     * Metodi get per getNace_r2
+     * @return
+     */
+    public String getNace_r2() {
+        return nace_r2;
+    }
 
-    public String getNace_r2() { return nace_r2; }
+    /**
+     * Metodi get per geo
+     * @return
+     */
+    public String getGeo() {
+        return geo;
+    }
 
-    public String getGeo() { return geo; }
-
+    /**
+     * Metodo toString che restituisce il record
+     * @return  restituisce il record sotto forma di stringa
+     */
     @Override
-    public String toString() {    //Metodo che restituisce la string del record
-        return "NottiNazione: " + "freq = " + freq + ", c_resid = " + c_resid + ", unit = " + unit + ", nace_r2 = " +  nace_r2 + ", geo = " + geo + ";" ;
+    public String toString() {
+        StringBuilder record;
+        record = new StringBuilder("NottiNazione: " + ", c_resid = " + c_resid + ", unit = " + unit + ", nace_r2 = " + nace_r2 + ", geo = " + geo + ";");
+        for (int i = 0; i < differenza_anni; i++)
+            record.append(" anno = ").append(2007 + i).append(" valori = ").append(valori[i]).append(";");
+        return record.toString();
     }
 }
