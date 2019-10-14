0cb08be585a1188782adacec00ff379d87b41cda
mo-98 <mo-98@hotmail.it>
2019-10-14 12:21:25 +0200

    Modifiche Filtri, aggiunti commenti

--- src/main/java/com/momoiaco/progetto/servizi/Filtri.java
+++ src/main/java/com/momoiaco/progetto/servizi/Filtri.java
@@ -8,23 +8,30 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Classe Filtri per gestione dei operatori di confronto e operatori logici.
+ */
 public abstract class Filtri {
+    // operatori di confronto
     private static final List<String> operatori = Arrays.asList("$not", "$in", "$nin", "$and", "$or", "$gt", "$gte", "$lt", "$lte", "$bt");
 
+    /**
+     *  Metodo il quale in base all'operatore inserito, il valore value e il riferimento reference vengono confrontati.
+     * @param value valore del campo
+     * @param operation operatore
+     * @param reference valore di riferimento
+     * @return boolean
+     */
 
     private static boolean check(Object value, String operation, Object reference) {
-        if (operatori.contains(operation)) {
-            if (value instanceof Number) {
-                double valueNum = ((Number) value).doubleValue();
-                if (reference instanceof Number) {
-                    double rifNum = ((Number) reference).doubleValue();
+        if (operatori.contains(operation)) { // verifica che l'operatore sia uno di quelli gestiti
+            if (value instanceof Number) { // caso in cui il valore da controllare sia un numero
+                double valueNum = ((Number) value).doubleValue();//cast in double
+                if (reference instanceof Number) {//caso in cui anche il riferimento sia un numero
+                    double rifNum = ((Number) reference).doubleValue();//cast in double
                     switch (operation) {
                         case "$not":
                             return valueNum != rifNum;
-                        /*case "$and":
-                            return  ;
-                        case "$or":
-                            return  ;*/
                         case "$gt":
                             return valueNum > rifNum;
                         case "$gte":
@@ -34,16 +41,16 @@ public abstract class Filtri {
                         case "$lte":
                             return valueNum <= rifNum;
                         default:
-                            String erroreOper = "L'operatore: '" + operation + "' risulta inadatto per gli operandi: '" + value + "' , '" + reference + "'";
-                            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, erroreOper);
+                            String erroreOper = "L'operatore: '" + operation + "' risulta non funzionante per gli operandi: '" + value + "' , '" + reference + "'";
+                            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, erroreOper);//restituisce il messaggio di errore in formato JSON
                     }
-                } else if (reference instanceof List) {
+                } else if (reference instanceof List) { // riferimento risulta essere una lista
                     List rifL = ((List) reference);
-                    if (!rifL.isEmpty() && rifL.get(0) instanceof Number) {
+                    if (!rifL.isEmpty() && rifL.get(0) instanceof Number) { // la lista deve essere non vuota e deve contenere numeri
 
-                        List<Double> leftReferenceNum = new ArrayList<>();
+                        List<Double> leftReferenceNum = new ArrayList<>();//conversione
                         for (Object elem : rifL) {
-                            leftReferenceNum.add(((Number) elem).doubleValue());
+                            leftReferenceNum.add(((Number) elem).doubleValue());// conversione di ogni singolo elemento
                         }
                         switch (operation) {
                             case "$in":
@@ -55,17 +62,17 @@ public abstract class Filtri {
                                 double second = leftReferenceNum.get(1);
                                 return valueNum >= first && valueNum <= second;
                             default:
-                                String erroreOper = "L'operatore: '" + operation + "' risulta inadatto per gli operandi: '" + value + "' , '" + reference + "'";
+                                String erroreOper = "L'operatore: '" + operation + "' risulta non funzionante  per gli operandi: '" + value + "' , '" + reference + "'";
                                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, erroreOper);
                         }
                     } else
                         throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Lista vuota o non numerica");
                 } else
                     throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Il riferimento: '" + reference + "' non è compatibile con il valore: '" + value + "'");
-            } else if (value instanceof String || value instanceof Character) {
+            } else if (value instanceof String || value instanceof Character) {// il valore da controllare è un carattere
                 if(value instanceof Character)
-                    value=String.valueOf(value);
-                String valueStr = ((String) value);
+                    value=String.valueOf(value);// riporto il carattere ad una stringa
+                String valueStr = ((String) value);//conversione
                 if (reference instanceof String) {
                     String rifStr = ((String) reference);
                     if (operation == "$not") {
@@ -75,9 +82,9 @@ public abstract class Filtri {
                             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, erroreOper);
                     }
                 } else {
-                    if (reference instanceof List) {
+                    if (reference instanceof List) {// il riferimento è una lista
                     List rifList = ((List) reference);
-                    if (!rifList.isEmpty() && rifList.get(0) instanceof String) {
+                    if (!rifList.isEmpty() && rifList.get(0) instanceof String) {//la lista deve essere non vuota e deve contenere stringhe per poter effettuare la conversione da una lista generica ad una lista di stringhe
                            List<String> rifLStr = new ArrayList<>();
                         String valueStr = ((String) value);
                         for (Object elem : rifList) {
@@ -89,30 +96,37 @@ public abstract class Filtri {
                             case "$nin":
                                 return !rifLStr.contains(valueStr);
                             default:
-                                String message = "L'operatore: '" + operation + "' risulta inadatto per gli operandi: '" + value + "' , '" + reference + "'";
+                                String message = "L'operatore: '" + operation + "' non funziona per gli operandi: '" + value + "' , '" + reference + "'";
                                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, message);
                         }
                     } else
-                        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "La lista è vuota o non contiene stringhe");
+                        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "La lista è vuota");
                 } else
                     throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Riferimento:'" + reference + "' non compatibile con il valore'" + value + "'");
             }
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Valore da controllare non valido: '" + value + "'");
         } else
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Operatore non valido: " + operation);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Operatore " + operation + " non è valido");
     }
 
     public static List<String> getOperatori() {
         return operatori;
     }
 
+    /**
+     * Metodo per l'applicazione dei filtri ad una lista.
+     * @param val lista dei valori
+     * @param oper operatore
+     * @param rif valore di riferimento
+     * @return lista che soddisfano il filtro
+     */
     public static List<Integer> filtra(List val, String oper, Object rif) {
         List<Integer> filtrati = new ArrayList<>();
         for (int i = 0; i < val.size(); i++) {
-            if (check(val.get(i), oper, rif))
+            if (check(val.get(i), oper, rif))//controllo per ogni elemento della lista
                 filtrati.add(i);
         }
-        return filtrati;
+        return filtrati; // restituisco la lista con gli indici
     }
 }
 
